ff9c59d68fb7f7e50eafd19eb523eb3a3e298882
karnandohuang <karnandohuang@outlook.com>
2026-03-27 22:50:39 +0700

    added useEffect hook to sort places based on user location

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useRef, useState } from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import {sortPlacesByDistance} from "./loc.js";
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+    const [sortedPlaces, setSortedPlaces] = useState([])
+
+    useEffect(() => {
+        window.navigator.geolocation.getCurrentPosition((position) => {
+            const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude)
+            setSortedPlaces(sortedPlaces);
+        })
+    }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -63,7 +72,7 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={sortedPlaces}
           onSelectPlace={handleSelectPlace}
         />
       </main>
