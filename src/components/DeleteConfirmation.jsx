018b4e9022820c4ab93cac6db3424cc49b6119fb
karnandohuang <karnandohuang@outlook.com>
2026-03-29 19:53:28 +0700

    add useCallback

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -11,7 +11,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
             console.log('cleanup')
             clearTimeout(timer)
         }
-    }, []);
+    }, [onConfirm]);
 
   return (
     <div id="delete-confirmation">
